3c44442d63bfac253ca38bdc50c546bd98e5c8cd
Takashi SASAKI <takashi316@gmail.com>
2017-12-10 15:41:44 +0900

    use generator in git-list-all.py

--- git-list-all.py
+++ git-list-all.py
@@ -1,44 +1,47 @@
 #!/usr/bin/python3 
-import git,sys,os,copy,argparse
+import git,sys,os,copy,argparse,json
 
 
 def doTree(tree, leadingPath):
   thisPath = copy.deepcopy(leadingPath)
   thisPath.append(tree.name)
-  print("T", thisPath)
+  yield {"type": tree.type, "path": thisPath}
 
   for o in tree:
     if isinstance(o, git.objects.submodule.base.Submodule):
-      doSubmodule(o, thisPath)
+      yield doSubmodule(o, thisPath)
     if isinstance(o, git.objects.tree.Tree):
       doTree(o, thisPath)
     if isinstance(o, git.objects.tree.Blob):
-      doBlob(o, thisPath)
+      yield doBlob(o, thisPath)
 
   for o in tree.trees:
     if isinstance(o, git.objects.submodule.base.Submodule):
-      doSubmodule(o, thisPath)
+      yield doSubmodule(o, thisPath)
     if o is not tree and isinstance(o, git.objects.tree.Tree):
-     doTree(o, thisPath)
+      doTree(o, thisPath)
     if isinstance(o, git.objects.blob.Blob):
-      doBlob(o, thisPath)
-
+      yield doBlob(o, thisPath)
 
 def doSubmodule(submodule, leadingPath):
   thisPath = copy.deepcopy(leadingPath)
   try: 
     thisPath.append(submodule.name)
-    print("S", thisPath)
+    return {"type": submodule.type, "path": thisPath}
   except Exception as e:
-    print(e)
+    return {"type": submodule.type, "path": thisPath} 
 
 def doBlob(blob, leadingPath):
   thisPath = copy.deepcopy(leadingPath)
   thisPath.append(blob.name)
-  print("B", thisPath)
+  return {"type": blob.type, "path": thisPath}
 
 def doCommit(commit):
-  doTree(commit.tree, [])
+  generator = doTree(commit.tree, [])
+  generator
+  for x in generator:
+    x["commit"] = commit.hexsha
+    print("\x1e"+json.dumps(x)+"\x0a")
 
 argumentParser = argparse.ArgumentParser(allow_abbrev=True)
 argumentParser.add_argument("gitDirectory", default=".")
