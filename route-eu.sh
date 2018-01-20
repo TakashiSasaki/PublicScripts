80f25a1e7bda2c4840e680e0a546df2fa3460dba
Takashi SASAKI <takashi316@gmail.com>
2018-01-20 17:34:27 +0900

    change sleep time to 60 seconds

--- route-eu.sh
+++ route-eu.sh
@@ -3,7 +3,7 @@ PATH=$HOME/bin:$PATH
 
 while true; do
 
-	sleep 20
+	sleep 60
 	sudo ip route del default dev tun0 scope link
 	#ip route del 133.71.254.4/32 dev eth0
 	sudo ip route add 133.71.3.0/24 dev tun0 scope link
