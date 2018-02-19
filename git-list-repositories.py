65fb2c7c76fd9062a3f0019e408c227b73ad4a21
Takashi SASAKI <takashi316@gmail.com>
2018-02-19 16:27:35 +0900

    going to use argparse in git-list-repositories.py

--- git-list-repositories.py
+++ git-list-repositories.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-import git,sys,os
+import git,sys,os,argparse
+
+parser = argparse.ArgumentParser(description="List git repositories.")
+parser.add_argument("pathFile", nargs=1)
+parser.add_argument("--stdin", action="store_true") 
+namespace = parser.parse_args()
+print(namespace)
 
 for x in sys.stdin:
   repoPath = os.path.abspath(x.strip())
